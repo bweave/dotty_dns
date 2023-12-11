c466274b999d3946207d3342f6be83bf4644f585
Brian Weaver <bdrums@gmail.com>
2023-12-11 15:47:12 -0500

    Add a user model
    And like I said, disallow sign up.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, skip: [:registrations], sign_out_via: [:get]
+  as :user do
+    get "users/edit" => "devise/registrations#edit",
+        :as => "edit_user_registration"
+    put "users" => "devise/registrations#update", :as => "user_registration"
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
