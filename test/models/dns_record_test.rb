6b85ab1048fd0be8afab947840a906569e21fba0
Brian Weaver <bdrums@gmail.com>
2023-12-15 08:11:36 -0500

    Add some handy scopes for wildcard DnsRecords

--- test/models/dns_record_test.rb
+++ test/models/dns_record_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class DnsRecordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test ".wildcards " do
+    expected = [dns_records(:wildcard)]
+    actual = described_class.wildcards
+    assert_equal expected, actual
+  end
+
+  test ".not_wildcards" do
+    expected = [dns_records(:one), dns_records(:two)]
+    actual = described_class.not_wildcards
+    assert_equal expected, actual
+  end
 end
